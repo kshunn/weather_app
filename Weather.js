1dfd2b3cc0d25b0d2d05ee293ce6eacf0d73c9f5
kshunn <qkshunn@gmail.com>
2020-03-03 21:39:42 +0900

    add weather icons above temperature

--- Weather.js
+++ Weather.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Weather({temp}){
+export default function Weather({temp, condition}){
     return (
         <View style={styles.container}>
-            <Text>
-                {temp}
-            </Text>
+            <View style={styles.halfContainer}>
+                <MaterialCommunityIcons size={96} name='weather-lightning-rainy'/>
+                <Text style={styles.temp}>{temp}C</Text>
+            </View>
+            <View style={styles.halfContainer}>
+
+            </View>
         </View>
     );
 }
 
 Weather.propTypes = {
-    temp: PropTypes.number.isRequired
+    temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        'ThunderStorm',
+        'Drizzle',
+        'Rain',
+        'Snow',
+        'Atmosphere',
+        'Clear',
+        'Clouds',
+        'Haze',
+        'Mist',
+        'Dust'
+    ]).isRequired
 };
 
 const styles = StyleSheet.create({
@@ -21,5 +39,13 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
+    halfContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    temp: {
+        fontSize: 42
+    },
 });
