f08789fe89bd9a07a818d692a4e6448c66b86f4c
kshunn <qkshunn@gmail.com>
2020-03-03 20:49:43 +0900

    loading screen, getting location information

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from 'react-native';
+import Loading from './Loading';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
-  );
+export default class extends React.Component {
+  state ={
+    isLoading: true
+  };
+  getLocation = async() => {
+    try{
+      await Location.requestPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.setState({isLoading: false});
+    } catch(error){
+      Alert.alert("Can't find you", "So sad");
+    }
+  };
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoading} = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
