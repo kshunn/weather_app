ca4f02e3415b1cb7e2d439cef8ab2673a86c6108
kshunn <qkshunn@gmail.com>
2020-03-03 22:13:30 +0900

    title, subtitle, gradient background + designs, status bar change

--- Weather.js
+++ Weather.js
@@ -1,20 +1,92 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 
+const weatherOptions = {
+    Thunderstorm: {
+      iconName: "weather-lightning",
+      gradient: ["#373B44", "#4286f4"],
+      title: "Thunderstorm",
+      subtitle: "Lorem ipsum dolor sit amet"
+    },
+    Drizzle: {
+      iconName: "weather-hail",
+      gradient: ["#89F7FE", "#66A6FF"],
+      title: "Drizzle",
+      subtitle: "Lorem ipsum dolor sit amet"
+    },
+    Rain: {
+      iconName: "weather-rainy",
+      gradient: ["#00C6FB", "#005BEA"],
+      title: "Rain",
+      subtitle: "Lorem ipsum dolor sit amet"
+    },
+    Snow: {
+      iconName: "weather-snowy",
+      gradient: ["#7DE2FC", "#B9B6E5"],
+      title: "Snow",
+      subtitle: "Lorem ipsum dolor sit amet"
+    },
+    Atmosphere: {
+      iconName: "weather-hail",
+      gradient: ["#89F7FE", "#66A6FF"],
+      title: "Atmosphere",
+      subtitle: "Lorem ipsum dolor sit amet"
+    },
+    Clear: {
+      iconName: "weather-sunny",
+      gradient: ["#FF7300", "#FEF253"],
+      title: "Clear",
+      subtitle: "Lorem ipsum dolor sit amet"
+    },
+    Clouds: {
+      iconName: "weather-cloudy",
+      gradient: ["#D7D2CC", "#304352"],
+      title: "Clouds",
+      subtitle: "Lorem ipsum dolor sit amet"
+    },
+    Mist: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Mist",
+      subtitle: "Lorem ipsum dolor sit amet"
+    },
+    Dust: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Dusty",
+      subtitle: "Lorem ipsum dolor sit amet"
+    },
+    Haze: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Haze",
+      subtitle: "Lorem ipsum dolor sit amet"
+    }
+  };
+
 export default function Weather({temp, condition}){
     return (
-        <View style={styles.container}>
+        <LinearGradient
+            colors={weatherOptions[condition].gradient}
+            style={styles.container}
+        >
+            <StatusBar barStyle='light-content' />
             <View style={styles.halfContainer}>
-                <MaterialCommunityIcons size={96} name='weather-lightning-rainy'/>
-                <Text style={styles.temp}>{temp}C</Text>
+                <MaterialCommunityIcons
+                    size={96}
+                    name={weatherOptions[condition].iconName}
+                    color='white'
+                />
+                <Text style={styles.temp}>{temp}°</Text>
             </View>
-            <View style={styles.halfContainer}>
-
+            <View style={{...styles.halfContainer, ...styles.textContainer}}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
             </View>
-        </View>
+        </LinearGradient>
     );
 }
 
@@ -36,9 +108,7 @@ Weather.propTypes = {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        flex: 1
     },
     halfContainer: {
         flex: 1,
@@ -46,6 +116,27 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     temp: {
-        fontSize: 42
+        fontSize: 42,
+        color: 'white'
     },
+    title: {
+        color: 'white',
+        fontSize: 44,
+        fontWeight: '300',
+        marginBottom: 10,
+        textAlign: "left"
+    },
+    subtitle: {
+        color: 'white',
+        fontWeight: '600',
+        fontSize: 24,
+        textAlign: "left"
+    },
+    textContainer: {
+        alignItems: "flex-start",
+        paddingHorizontal: 40,
+        justifyContent: 'center',
+        flex: 1
+    }
+
 });
